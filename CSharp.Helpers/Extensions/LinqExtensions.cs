8cb14f7a2ffb037fdd518187e4efb50a72005d54
abdallah <abdallah@KOON>
2017-02-08 12:28:07 +0100

    Projektdateien hinzufügen.

---
+++ CSharp.Helpers/Extensions/LinqExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharp.Helpers.Extensions
+{
+    public static class LinqExtensions
+    {
+        public static IEnumerable<T> Except<T, TKey>(this IEnumerable<T> items, IEnumerable<T> other, Func<T, TKey> getKey)
+        {
+            if (items == null)
+                return null;
+
+            if (other == null)
+                return items;
+
+            return from item in items
+                   join otherItem in other on getKey(item)
+                   equals getKey(otherItem) into tempItems
+                   from temp in tempItems.DefaultIfEmpty()
+                   where ReferenceEquals(null, temp) || temp.Equals(default(T))
+                   select item;
+        }
+
+        /// <summary>
+        /// For example:
+        /// var query = people.DistinctBy(p => p.Id);
+        /// var query = people.DistinctBy(p => new { p.Id, p.Name });
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="keySelector"></param>
+        /// <returns></returns>
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            HashSet<TKey> seenKeys = new HashSet<TKey>();
+            foreach (TSource element in source)
+            {
+                if (seenKeys.Add(keySelector(element)))
+                {
+                    yield return element;
+                }
+            }
+        }
+
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> items)
+        {
+            return items == null || !items.Any();
+        }
+
+        public static bool IsNotNullAndAny<T>(this IEnumerable<T> items, Func<T, bool> predicate)
+        {
+            return items != null && items.Any(predicate);
+        }
+    }
+}
---
+++ CSharp.Helpers/Extensions/StringExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CSharp.Helpers.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool CompareTo(this string str, string secondString, bool IgnoreCaseSensitive)
+        {
+            if (string.IsNullOrEmpty(str) && string.IsNullOrEmpty(secondString))
+                return true;
+            else if (!string.IsNullOrEmpty(str) && string.IsNullOrEmpty(secondString)
+                || string.IsNullOrEmpty(str) && !string.IsNullOrEmpty(secondString))
+                return false;
+            else
+            {
+                if (IgnoreCaseSensitive)
+                {
+                    return str.ToLower() == secondString.ToLower();
+                }
+                else
+                    return str == secondString;
+            }
+        }
+
+        public static string RemoveSpecialCharacters(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return string.Empty;
+
+            return Regex.Replace(str, "[^0-9a-zA-Z]+", "", RegexOptions.Compiled);
+        }
+
+        public static string SubstringExtended(this string str, int startIndex, int length )
+        {
+            if (string.IsNullOrEmpty(str))
+                return string.Empty;
+            if (str.Length < length)
+                return str;
+
+            return str.Substring(startIndex,length);
+        }
+
+        public static string ToLowerExtended(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return string.Empty;
+
+            return str.ToLower();
+        }
+    }
+
+}
