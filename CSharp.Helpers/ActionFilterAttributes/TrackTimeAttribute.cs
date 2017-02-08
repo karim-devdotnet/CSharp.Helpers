0ee45f6443e605b3df1812b0da9bc3a282abca67
karimabd84 <abdelkarim.esti@gmail.com>
2017-02-08 14:16:41 +0100

    TrackTimeAttribute

---
+++ CSharp.Helpers/ActionFilterAttributes/TrackTimeAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using System.Web.Mvc;
+
+namespace CSharp.Helpers.ActionFilterAttributes
+{
+    /// <summary>
+    /// an action filter attribute that stores how much time has taken to execute an action.
+    /// </summary>
+    public class TrackTimeAttribute : ActionFilterAttribute
+    {
+        Stopwatch stopWatch;
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            stopWatch = new Stopwatch();
+            stopWatch.Start();
+            base.OnActionExecuting(filterContext);
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            stopWatch.Stop();
+            Trace.WriteLine("Elapsed = " + stopWatch.ElapsedMilliseconds);
+            base.OnResultExecuted(filterContext);
+        }
+
+    }
+}
