de03b94c09be514570f0c0cc6f2f2dc1bacdadd8
karimabd84 <abdelkarim.esti@gmail.com>
2017-02-09 11:13:53 +0100

    FileInfoExtensions added

---
+++ CSharp.Helpers/Extensions/FileInfoExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace CSharp.Helpers.Extensions
+{
+    public static class FileInfoExtensions
+    {
+        public static bool IsInUse(this FileInfo file)
+        {
+            FileStream stream = null;
+
+            try
+            {
+                stream = file.Open(FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+            }
+            catch (Exception)
+            {
+                //the file is unavailable because it is:
+                //still being written to
+                //or being processed by another thread
+                //or does not exist (has already been processed)
+                return true;
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+            }
+            return false;
+        }
+    }
+}
