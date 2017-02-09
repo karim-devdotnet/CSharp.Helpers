1310e3dc4c16175dba30202fdf199d0245bd25d5
karimabd84 <abdelkarim.esti@gmail.com>
2017-02-09 11:12:13 +0100

    RegexHelper added

---
+++ CSharp.Helpers/Helpers/RegexHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CSharp.Helpers.Helpers
+{
+    public class RegexHelper
+    {
+        public static string RemoveSpecialCharacters(string input)
+        {
+            return Regex.Replace(input, "[^a-zA-Z0-9]+", "", RegexOptions.Compiled);
+        }
+    }
+}
