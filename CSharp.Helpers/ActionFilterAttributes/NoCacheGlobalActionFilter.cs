dc5ab3e00e6941c50f1429e6c21235d9623c1252
karimabd84 <abdelkarim.esti@gmail.com>
2017-02-09 10:07:48 +0100

    NoCacheGlobalActionFilter added

---
+++ CSharp.Helpers/ActionFilterAttributes/NoCacheGlobalActionFilter.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CSharp.Helpers.ActionFilterAttributes
+{
+    public class NoCacheGlobalActionFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.ActionDescriptor.GetCustomAttributes(typeof(OutputCacheAttribute), false).Length > 0)
+                return;
+
+            HttpCachePolicyBase cache = filterContext.HttpContext.Response.Cache;
+            cache.SetCacheability(HttpCacheability.NoCache);
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
